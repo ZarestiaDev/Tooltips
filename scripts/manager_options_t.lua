9b447012c7814e854743fd74d68c9bfaf7b594c5
ZarestiaDev <6247787+ZarestiaDev@users.noreply.github.com>
2023-02-07 20:41:57 +0100

    2023-02 ruleset updates, all main

--- scripts/manager_options_t.lua
+++ scripts/manager_options_t.lua
@@ -3,6 +3,6 @@ function onInit()
 end
 
 function registerOptionsT()
-    OptionsManager.registerOption2("TSOS",true, "option_header_client", "option_label_TSOS", "option_entry_cycler", 
+    OptionsManager.registerOption2("TSOS",true, "option_header_tooltip", "option_label_TSOS", "option_entry_cycler", 
     { labels = "option_val_on", values = "on", baselabel = "option_val_off", baseval = "off", default = "off" });
 end
